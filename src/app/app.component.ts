3fd1cbd2f64aede85aa1d32fcf4db3c5203e1bd4
Rafi <rafi.bogra@gmail.com>
2017-07-21 18:34:49 +0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  themes = [{
+    name: 'dark-pink',
+    baseColor: '#e91e63',
+    isActive: false
+  }, {
+    name: 'indigo',
+    baseColor: '#3f51b5',
+    isActive: true
+  }];
+
+
+  changeTheme(themeName: string) {
+    this.themes.forEach((theme) => {
+      this.removeClass(document.body, theme.name);
+      theme.isActive = false;
+      if(theme.name === themeName)
+        theme.isActive = true;
+    });
+    this.addClass(document.body, themeName);
+  };
+  removeClass(el, className) {
+    if (!el || el.length === 0)
+      return;
+    if (!el.length) {
+      el.classList.remove(className);
+    } else {
+      for (var i = 0; i < el.length; i++) {
+        el[i].classList.remove(className)
+      }
+    }
+  }
+  addClass(el, className) {
+    if (!el)
+      return;
+    if (!el.length) {
+      el.classList.add(className);
+    } else {
+      for (var i = 0; i < el.length; i++) {
+        el[i].classList.add(className);
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,8 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FlexLayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
